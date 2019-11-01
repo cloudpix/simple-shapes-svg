159ff778586a857d041929a46dc0aebf11a12e9d
Rafael Cruz <rafael.cruz@iscoolapp.com.br>
2019-11-01 09:57:22 -0300

    - categories icons. - module export.

--- index.js
+++ index.js
@@ -1,1 +1,14 @@
-module.exports = {};
+'use strict';
+
+const catalog = require('./data/catalog.json');
+const images = require.context('./min/', true, /^\.\/.*\.(svg)$/i);
+
+function loadImages() {
+	catalog.forEach(c => c.images.forEach(i => i.file = images(c.path + i.file)));
+}
+
+loadImages();
+
+module.exports = {
+	getCategories: () => catalog.concat()
+};
